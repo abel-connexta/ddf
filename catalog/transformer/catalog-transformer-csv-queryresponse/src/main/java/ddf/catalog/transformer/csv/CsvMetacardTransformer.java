5c3c84955d2632ab02b2045869a4e9b7b4d902d2
Abel Amadou <abel.amadou@connexta.com>
2021-02-01 08:58:26 -0700

    reverted CsvMetacardTransformer and address CqlTransformHandler comment

--- catalog/transformer/catalog-transformer-csv-queryresponse/src/main/java/ddf/catalog/transformer/csv/CsvMetacardTransformer.java
+++ catalog/transformer/catalog-transformer-csv-queryresponse/src/main/java/ddf/catalog/transformer/csv/CsvMetacardTransformer.java
@@ -31,14 +31,12 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CsvMetacardTransformer implements MetacardTransformer {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CsvMetacardTransformer.class);
-  public static final String COLUMN_ORDER = "columnOrder";
 
   @Override
   public BinaryContent transform(Metacard metacard, Map<String, Serializable> arguments)
